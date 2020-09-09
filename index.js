034aeab3bf27f77f28eea22b9943b02278ba2d3a
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2020-09-09 12:42:24 +0700

    Add each string format in js

---
+++ index.js
@@ -0,0 +1,14 @@
+const name = 'Micky'
+console.log(`"My name is ${name}."\n"I'm a ${1*2*3*4 + 1} year-old"`)
+
+console.log(`I'm not
+sure that you think
+about me`)
+
+console.log('Please do follow up "this guide"')
+console.log("Please do follow up 'this guide'")
+console.log("with double quote \"this guide\"")
+console.log('with single quote \'this guide\'')
+
+console.log('Try to write newline \\r\\n \r\n: after that?')
+console.log('Try to write newline \\n \n: after that?')
